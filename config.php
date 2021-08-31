7d35137e7fc5600fcbe9129beedb8ff0c3ea3909
Bonnie <61726588+chirinekill@users.noreply.github.com>
2021-08-31 17:52:39 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-$DBHOST = getenv('DBHOST'); // Set your database host here
-$DBUSER = getenv('DBUSER'); // Set your database user here
-$DBNAME = getenv('DBNAME'); // Set your database name here
-$DBPW =  getenv('DBPASS'); // Set your database password here
-$BOT_ID = getenv('BOT_ID');
-$TOKEN = getenv('BOT_TOKEN');
+$DBHOST = "localhost";
+$DBUSER = "id17506633_hoavangbox";
+$DBNAME = "id17506633_boxchatofhoavang";
+$DBPW =  'ViOx3PbE!vx0$_iu';
+$BOT_ID = "612cda43b415b92ada2c1cb3";
+$TOKEN = "HtFGsgmByeIMJef2kNU6Ky4Z45cYk8Ud244SarpLYFEO6JtlyE9EOf8fAUF2xMso";
 
-$NUDE_API_USER = getenv('NUDE_API_USER');
-$NUDE_API_SECRET = getenv('NUDE_API_SECRET');
+$NUDE_API_USER = "NUDE_API_USER";
+$NUDE_API_SECRET = "NUDE_API_SECRET";
 
 $BLOCK_NAME = "uet_chat";
 $JSON = "uet_text";
